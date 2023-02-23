45404ace70c5944ec3978e4302c3e7eea9b25621
loc.le4 <loc.le4@mservice.com.vn>
2023-02-23 16:20:53 +0700

    f

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>
-        Go to `pages/_app.js` and `pages/_document.js` to see how you can add
-        Google Tag Manager to your app
+      GA2
       </h1>
       <Link href={"/about"}>
       go to about
